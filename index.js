eabbb1ea9474e5023c35c575a25b595a63070cb3
Yoav Lahav <yoavl@wix.com>
2018-04-23 13:03:25 +0300

    changed index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
 
     /** UPDATE YOUR VERIFY TOKEN **/
-    const VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>";
+    const VERIFY_TOKEN = "garage99";
 
     // Parse params from the webhook verification request
     let mode = req.query['hub.mode'];
