f21222e4bef7fe018c106cc3b57219f9501aff2a
Yoav Lahav <yoavl@wix.com>
2018-04-23 16:16:45 +0300

    changed index.js

--- index.js
+++ index.js
@@ -14,34 +14,23 @@ const
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
 // Accepts POST requests at /webhook endpoint
-app.post('/webhook', (req, res) => {
-
-    // Parse the request body from the POST
-    let body = req.body;
-
-    // Check the webhook event is from a Page subscription
-    if (body.object === 'page') {
-
-        // Iterate over each entry - there may be multiple if batched
-        body.entry.forEach(function(entry) {
-
-            // Get the webhook event. entry.messaging is an array, but
-            // will only ever contain one event, so we get index 0
-            let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
-
+app.post("/webhook", function (req, res) {
+    // Make sure this is a page subscription
+    if (req.body.object == "page") {
+        // Iterate over each entry
+        // There may be multiple entries if batched
+        req.body.entry.forEach(function(entry) {
+            // Iterate over each messaging event
+            entry.messaging.forEach(function(event) {
+                if (event.postback) {
+                    processPostback(event);
+                }
+            });
         });
 
-        // Return a '200 OK' response to all events
-        res.status(200).send('EVENT_RECEIVED');
-
-    } else {
-        // Return a '404 Not Found' if event is not from a page subscription
-        res.sendStatus(404);
+        res.sendStatus(200);
     }
-
 });
-
 // Accepts GET requests at the /webhook endpoint
 app.get('/webhook', (req, res) => {
 
